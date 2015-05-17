2949b5a1b2ee203c3ceb047de0486203e8e8c2b7
azagh2084 <johannes.bonenberger@gmx.net>
2015-05-17 20:29:00 +0200

    init

---
+++ Assets/scripts/CameraScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+		if(Input.GetKey("a")){
+			//	float inputX = Input.GetAxis("Mouse ScrollWheel");
+			GetComponent<Camera> ().transform.Rotate(Vector3.up * -2F);
+
+			//rot.y+=(float)0.05;
+			//GetComponent<Camera> ().transform.rotation= rot;
+		}
+		if(Input.GetKey("d")){
+			//	float inputX = Input.GetAxis("Mouse ScrollWheel");
+			GetComponent<Camera> ().transform.Rotate(Vector3.up * 2F);
+		}
+		if(Input.GetKey("w")){
+
+			GetComponent<Camera> ().transform.position += GetComponent<Camera> ().transform.forward * 0.05F *1;
+
+		}if(Input.GetKey("s")){
+			GetComponent<Camera> ().transform.position += GetComponent<Camera> ().transform.forward * 0.05F *-1;
+		}
+		
+		if(Input.GetKey("o")){
+			//	float inputX = Input.GetAxis("Mouse ScrollWheel");
+			GetComponent<Camera> ().orthographicSize = GetComponent<Camera> ().orthographicSize + 0.1F;
+		}if(Input.GetKey("k")){
+			//	float inputX = Input.GetAxis("Mouse ScrollWheel");
+			GetComponent<Camera> ().orthographicSize = GetComponent<Camera> ().orthographicSize - 0.1F;
+		}
+		
+		
+		
+		float inputX = Input.GetAxis("Mouse ScrollWheel");
+		if (inputX != 0) {
+			if (GetComponent<Camera> ().orthographicSize+( 10 * inputX)>0)		{	
+				GetComponent<Camera> ().orthographicSize = GetComponent<Camera> ().orthographicSize + 2 * inputX;
+			}
+			if (GetComponent<Camera> ().orthographicSize<=0){
+				GetComponent<Camera> ().orthographicSize=0;
+			}
+		}
+		
+	}
+}
---
+++ Assets/scripts/LabyrinthCreator.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LabyrinthCreator : MonoBehaviour {
+
+	public GameObject floor;
+	public GameObject wall;
+
+	int[,] lab = { 
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{ 1, 0, 1, 1, 0, 1, 1, 0, 0, 1},
+		{ 1, 0, 1, 0, 0, 1, 1, 0, 0, 1},
+		{ 1, 0, 1, 1, 0, 0, 0, 0, 0, 1},
+		{ 1, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+		{ 1, 0, 0, 0, 1, 1, 1, 0, 0, 1},
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		};
+
+	void Start () {
+
+		for(int i=0;i<10;i++){
+			for(int j=0;j<10;j++){
+
+
+				if (lab[i,j]==0){				
+					GameObject obj= (GameObject)Instantiate(floor,new Vector3(i-5,0,j-5),Quaternion.identity);		
+				}else{
+					Instantiate(wall,new Vector3(i-5,1,j-5),Quaternion.identity);
+				}
+			}
+		}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
